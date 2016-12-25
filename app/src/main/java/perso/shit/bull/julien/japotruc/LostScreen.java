c94facf957d3a00d911cffe39bbccc5cdd5ae69c
Julien <molasjulien@gmail.com>
2016-12-25 23:59:28 +0100

    creating bone for simple app

---
+++ app/src/main/java/perso/shit/bull/julien/japotruc/LostScreen.java
@@ -0,0 +1,13 @@
+package perso.shit.bull.julien.japotruc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LostScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_screen);
+    }
+}
