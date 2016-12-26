dc2c69f830cb5c508784cbd938f974474d2036a3
Julien <molasjulien@gmail.com>
2016-12-26 16:43:48 +0100

    --Adding a bit of song -- Not complete though, just a test

--- app/src/main/java/perso/shit/bull/julien/japotruc/GameScreen.java
+++ app/src/main/java/perso/shit/bull/julien/japotruc/GameScreen.java
@@ -1,13 +1,18 @@
 package perso.shit.bull.julien.japotruc;
 
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.logging.Level;
 
@@ -17,23 +22,46 @@ public class GameScreen extends BackToWelcome {
 
     private GameSession session;
 
-    private TextView scoreTextVew;
+    private TextView scoreTextView;
+
+    private MediaPlayer mediaPlayerWin, mediaPlayerLose;
+
+    private ImageView japochiDisplay;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mediaPlayerWin = MediaPlayer.create(getApplicationContext(), R.raw.japosiffle1);
+        mediaPlayerLose = MediaPlayer.create(getApplicationContext(), R.raw.japosiffle2);
+        japochiDisplay = (ImageView) findViewById(R.id.japochiDisplay);
+        japochiDisplay.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.get)
+                return false;
+            }
+        });
         setContentView(R.layout.activity_game_screen);
-        scoreTextVew = (TextView)findViewById(R.id.scoreText);
-        scoreTextVew.setText(0);
+        scoreTextView = (TextView) findViewById(R.id.scoreText);
+        scoreTextView.setText(String.valueOf(0));
+
         session = new GameSession();
         setJapochiImage(session.getCurrentImage());
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
     /**
      * Called each time the user provides a good answer
      */
-    private void switchImage(){
+    private void switchImage() {
 
     }
 
@@ -42,15 +70,16 @@ public class GameScreen extends BackToWelcome {
      * If yes then launch display of new picture
      * If not then launch looser activity
      */
-    public void checkChinese(View view){
-        if(session.isCurrentJapanese()){
+    public void checkChinese(View view) {
+        if (session.isCurrentJapanese()) {
             //go to looser
+            mediaPlayerLose.start();
             gotToLost(view);
-        }
-        else{
+        } else {
             // carry ON
-            scoreTextVew.setText(session.getScore());
+            mediaPlayerWin.start();
             session.nextImage();
+            scoreTextView.setText(String.valueOf(session.getScore()));
             setJapochiImage(session.getCurrentImage());
         }
     }
@@ -59,40 +88,77 @@ public class GameScreen extends BackToWelcome {
      * Check if the picture is Japanese
      * If yes then launch display of new pic
      * If not then lauch looser activity
+     *
      * @param view
      */
-    public void checkJapanese(View view){
-        if(session.isCurrentJapanese()){
+    public void checkJapanese(View view) {
+        if (session.isCurrentJapanese()) {
             //continue
-            scoreTextVew.setText(session.getScore());
+            mediaPlayerWin.start();
             session.nextImage();
+            scoreTextView.setText(String.valueOf(session.getScore()));
             setJapochiImage(session.getCurrentImage());
-        }
-        else{
+        } else {
             //go to looser
+            mediaPlayerLose.start();
             gotToLost(view);
         }
     }
 
     /**
      * Set the background image of the imageView to imageName
+     *
      * @param imageName
      */
-    private void setJapochiImage(String imageName){
-        ImageView japochiDisplay = (ImageView) findViewById(R.id.japochiDisplay);
-        int imageId = getResources().getIdentifier(imageName , "drawable", getPackageName());
+    private void setJapochiImage(String imageName) {
+        int imageId = getResources().getIdentifier(imageName, "drawable", getPackageName());
         japochiDisplay.setBackgroundResource(imageId);
     }
 
-    public void gotToLost(View view){
+    public void gotToLost(View view) {
         Intent intent = new Intent(this, LostScreen.class);
         startActivity(intent);
-        myLogger.log(Level.INFO, "Go to Lost view from "+ view.getClass().toString());
+        myLogger.log(Level.INFO, "Go to Lost view from " + view.getClass().toString());
     }
 
     public void tryAgain() {
-        scoreTextVew.setText(0);
+        scoreTextView.setText(String.valueOf(0));
         this.session = new GameSession();
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("GameScreen Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
